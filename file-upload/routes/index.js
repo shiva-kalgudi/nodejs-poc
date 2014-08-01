a34ab081ffb302e1cbef0ea53c6e4cea2dd378b8
Shiva Kalgudi <shiva.kalgudi@yahoo.com>
2014-08-01 21:52:37 +0530

    initial commit

---
+++ file-upload/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res) {
+	console.dir(req.files);
+	res.redirect("/")
+});
+
+module.exports = router;
